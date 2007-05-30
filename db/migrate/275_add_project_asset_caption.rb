6bdcc96b1d8de1d15def0fdd3a49e68f19ac33cd
Robert Shell <rshell@edge-ka.com>
2007-05-30 11:33:38 +0000

    ok wrong table

--- db/migrate/275_add_project_asset_caption.rb
+++ db/migrate/275_add_project_asset_caption.rb
@@ -1,10 +1,10 @@
 class AddProjectAssetCaption < ActiveRecord::Migration
   def self.up
-    rename_column :project_contents,:content_data, :caption
-    change_column :project_contents,:caption,:text
+    rename_column :project_assets,:content_data, :caption
+    change_column :project_assets,:caption,:text
   end
 
   def self.down
-    rename_column :project_contents, :caption, :content_data
+    rename_column :project_assets, :caption, :content_data
   end
 end
