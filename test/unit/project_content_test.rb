d2b0ba1a4e6d16bac968299dc065332176b61d48
Robert Shell <rshell@edge-ka.com>
2007-12-12 15:48:40 +0000

    added a few extras to project_elements tests, need new fixtures to sorted before doing more tests

--- test/unit/project_content_test.rb
+++ test/unit/project_content_test.rb
@@ -3,8 +3,24 @@ require File.dirname(__FILE__) + '/../test_helper'
 class ProjectContentTest < Test::Unit::TestCase
   ## Biorails::Dba.import_model :project_contents
 
-  # Replace this with your real tests.
+  def setup
+    # Retrieve ## Biorails::Dba.import_model via their name
+     @model = ProjectContent
+  end
+  
   def test_truth
     assert true
   end
+  
+  def test_find
+     first = @model.find(:first)
+     assert first.id
+     assert first.name
+  end
+    
+  def test_has_name
+    first = @model.find(:first)
+    assert first.name    
+  end
+ 
 end
--- test/unit/project_element_test.rb
+++ test/unit/project_element_test.rb
@@ -3,8 +3,59 @@ require File.dirname(__FILE__) + '/../test_helper'
 class ProjectElementTest < Test::Unit::TestCase
   ## Biorails::Dba.import_model :project_elements
 
-  # Replace this with your real tests.
+  def setup
+    # Retrieve ## Biorails::Dba.import_model via their name
+     @model = ProjectElement
+  end
+  
   def test_truth
     assert true
   end
+  
+  def test_find
+     first = @model.find(:first)
+     assert first.id
+     assert first.name
+  end
+    
+  def test_has_name
+    first = @model.find(:first)
+    assert first.name    
+  end
+
+  def test_has_description
+    first = @model.find(:first)
+    assert first.description    
+  end
+
+   def test_has_summary
+    first = @model.find(:first)
+    assert first.description    
+  end
+
+   def test_has_title
+    first = @model.find(:first)
+    assert first.title    
+  end
+
+  def test_can_render_as_html
+    first = @model.find(:first)
+    assert first.to_html   
+  end
+
+  def test_has_style
+    first = @model.find(:first)
+    assert first.style   
+  end
+   
+  def test_has_icon
+    first = @model.find(:first)
+    assert first.icon
+  end
+
+  def test_has_path
+    first = @model.find(:first)
+    assert first.path
+  end
+   
 end
