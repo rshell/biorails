86859173fa4bbebc838da60cd48ce4e33111c532
Robert Shell <rshell@edge-ka.com>
2007-12-21 17:44:12 +0000

    unfrozen version of rails to allow fro 2.0.2

--- config/environment.rb
+++ config/environment.rb
@@ -9,7 +9,7 @@
 # Should work under 2.0 or 1.2 rails unlocked as testing with 2.0 at present
 # 
 # RAILS_GEM_VERSION = '1.2.6'
-RAILS_GEM_VERSION = '2.0.1'
+#RAILS_GEM_VERSION = '2.0.1'
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
